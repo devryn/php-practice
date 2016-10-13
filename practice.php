f131bc86d2851ad49a661a47d38e8713b4da25cb
Kate Walters <kawalters01@gmail.com>
2016-10-13 16:03:12 -0500

    How to write a string

---
+++ practice.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	</head>
+	<body>
+        <p>
+          <?php
+            echo "My name is Kate";
+          ?>
+        </p>
+	</body>
+</html>
+
+<!-- puts "My name is Kate" -->
