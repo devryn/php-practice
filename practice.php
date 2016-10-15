24f4eda2ef927789451ed08021dd9c2cd14bc978
Kate Walters <kawalters01@gmail.com>
2016-10-14 21:17:14 -0500

    Falling through statement

--- practice.php
+++ practice.php
@@ -31,6 +31,27 @@
         default:
             echo "Don't go comparing apples to oranges!";
     }
+
+    $i = 5; //falling through statement: checks multiple cases
+
+    switch ($i) {
+    case 0:
+        echo '$i is 0.';
+        break;
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        echo '$i is somewhere between 1 and 5.';
+        break;
+    case 6:
+    case 7:
+        echo '$i is either 6 or 7.';
+        break;
+    default:
+        echo "I don't know how much \$i is.";
+    }
           ?>
         </p>
 	</body>
