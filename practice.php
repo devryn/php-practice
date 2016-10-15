7b87bf13e6e222bb81f8a76a98ae7366be8518d3
Kate Walters <kawalters01@gmail.com>
2016-10-14 21:14:27 -0500

    Switch statement

--- practice.php
+++ practice.php
@@ -10,7 +10,8 @@
             echo 9 * 19 //multiplication
             $myName = "Kate"; //setting a variable
             echo $myName; //prints variable
-            10 <= 11;
+            10 <= 11; //comparing numbers
+
             $month = 10;
             if($month >= 12) {
               echo "It's almost Christmas!";
@@ -18,6 +19,18 @@
             else {
               echo "It's not close to Christmas.";
             }
+
+            $fruit = 'Apple'; //switch statement: if true, echos string and breaks. if false, goes to next case. default is if all cases are false.
+            switch ($fruit) {
+        case 'Apple':
+            echo "Apple.";
+            break;
+        case 'Orange';
+            echo "Orange";
+            break;
+        default:
+            echo "Don't go comparing apples to oranges!";
+    }
           ?>
         </p>
 	</body>
